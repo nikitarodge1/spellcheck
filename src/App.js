a2d626dfa38720af72798dc620406c4d62c57e76
Your Name <you@example.com>
2024-06-08 16:28:15 +0530

    spellcheck

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customDictionary = { teh: 'the', wrok: 'work', fot: 'for', exampl: 'example' };
+
+function SpellCheck() {
+    const [search, setSearch] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    useEffect(() => {
+        const parseSearchArray = search.split(" ");
+        const newSuggestions = [];
+
+        for (let i = 0; i < parseSearchArray.length; i++) {
+            let item = parseSearchArray[i].toLowerCase();
+            if (Object.prototype.hasOwnProperty.call(customDictionary, item)) {
+                newSuggestions.push(customDictionary[item]);
+            }
+        }
+
+        setSuggestions(newSuggestions);
+    }, [search]);
+
+    return (
+        <div>
+            <h2>Spell Check and Auto Correction</h2>
+            <textarea
+                cols="30"
+                rows="10"
+                value={search}
+                placeholder="Enter text..."
+                onChange={handleChange}
+            ></textarea>
+            {suggestions.length > 0 && (
+                <p>
+                    Did you mean:{" "}
+                    <strong>
+                        {suggestions.map((suggestion, index) => (
+                            <span key={index}>{suggestion}</span>
+                        ))}
+                    </strong>
+                    ?
+                </p>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default SpellCheck;
